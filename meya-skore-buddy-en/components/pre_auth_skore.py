1266ea5674197d80c779933f1cdde87ebb8dcc6e
Luan Naufal <luan.naufal@gmail.com>
2019-03-14 00:29:32 -0300

    ajustment for schedule logic

--- meya-skore-buddy-en/components/pre_auth_skore.py
+++ meya-skore-buddy-en/components/pre_auth_skore.py
@@ -4,18 +4,14 @@ from meya import Component
 class PreAuthSkore(Component):
     
     def start(self):
-        
-        tokenInfo = self.db.bot.get("skore_token")
-        
-        # If token still valid, then use it to avoid re-login
-        if not tokenInfo or not Skore.tokenValid(tokenInfo["expiration"]):
-            email = self.db.bot.settings["skore_user"]
-            password = self.db.bot.settings["skore_key"]
-            print("email", email)
-            print("password", password)
-            
-            skore = Skore(email = email, password = password)
     
-            self.db.bot.set("skore_token", {"token": skore.token, "expiration": skore.tokenExpiration})
+        email = self.db.bot.settings["skore_user"]
+        password = self.db.bot.settings["skore_key"]
+        print("email", email)
+        print("password", password)
+        
+        skore = Skore(email = email, password = password)
+
+        self.db.bot.set("skore_token", {"token": skore.token, "expiration": skore.tokenExpiration})
 
         return self.respond(message = None, action = "next")

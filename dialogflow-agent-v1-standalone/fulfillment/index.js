c37c0edb56c47d9895233ebef2ad5edc0cbfb1aa
Luan Naufal <luan.naufal@gmail.com>
2019-03-18 13:24:56 -0300

    Update dialogflow agent

--- dialogflow-agent-v1-standalone/fulfillment/index.js
+++ dialogflow-agent-v1-standalone/fulfillment/index.js
@@ -24,17 +24,17 @@ function requestLoginSkore(){
     method: "POST",
     body: {
       email: "luan.naufal@gmail.com",
-      password: "PASSWORD",
+      password: "d5%$#3j3",
       company_id: "5704"
     },
     headers: {
         "Content-type": "application/json",
-		"Accept": "text/plain"
+    "Accept": "text/plain"
     },
     json: true,
     transform: function(body, response){
-    	return body.token;
-	},
+      return body.token;
+  },
   };
   return rp( options );
 }
@@ -44,37 +44,38 @@ function requestMaterialsSkore(token){
     uri: "https://knowledge.skore.io/workspace/v2/spaces/34137/contents",
     headers: {
         "Content-type": "application/json",
-		"Accept": "text/plain",
-      	"Authorization": token
+    "Accept": "text/plain",
+        "Authorization": token
     },
     json: true,
     transform: function(body, response){
-    	var output = [];
-    	body.results.forEach(element => {
-    		output.push(element.name);
-    	});
-    	console.log(output);
-    	return output;
-	},
+      var output = [];
+      body.results.forEach(element => {
+        output.push(element.name);
+      });
+      console.log(output);
+      return output;
+  },
   };
   return rp( options );
 }
 
 app.intent(MATERIALS, (conv) => {
-	return requestLoginSkore()
-  	.then( body => {
+  return requestLoginSkore()
+    .then( body => {
         log.write(log.entry(METADATA, {message: "Login output"}));
         return requestMaterialsSkore(body)
-      	.then( materials => {
+        .then( materials => {
             log.write(log.entry(METADATA, {message: "Gettings materials output"}));
             log.write(log.entry(METADATA, {message: materials}));
             conv.ask(new SimpleResponse ({
               speech: "Those are the materials available: " + materials.join(", "),
               text: "Those are the materials available:  \n- " + materials.join("  \n- "),
             }));
+            conv.ask(new Suggestions('Open Jobs', 'Help'));
             return Promise.resolve(conv);
       })
-      	.catch( err => {
+        .catch( err => {
             log.write(log.entry(METADATA, {message: err}));
             return Promise.resolve(conv);
         }); 
@@ -83,8 +84,8 @@ app.intent(MATERIALS, (conv) => {
         log.write(log.entry(METADATA, {message: err}));
         return Promise.resolve(conv);
     }); 
-  	//return Promise.resolve(conv);
-	//return requestMaterialsSkore(conv, token);
+    //return Promise.resolve(conv);
+  //return requestMaterialsSkore(conv, token);
 });
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
